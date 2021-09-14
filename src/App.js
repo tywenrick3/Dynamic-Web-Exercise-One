cdb69b5d3706aa51998ed88d7ea6c677fe397a1c
Ty Wenrick <tew287@nyu.edu>
2021-09-14 08:57:30 -0400

    Routers n Switch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Article from './containers/Article'
+import ArticleListing from './containers/ArticleListing'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/article">
+          <Article/>
+        </Route>
+        <Route path="/">
+          <ArticleListing/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
