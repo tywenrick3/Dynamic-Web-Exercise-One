c732c0203cbd0ad3df49bd9b156e629ce7a876ec
Ty Wenrick <tew287@nyu.edu>
2021-09-14 08:42:08 -0400

    containers

---
+++ src/containers/Article.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Article() {
+    const articleTitle = 'Single Article'
+    return (
+        <div>
+            <h1>{articleTitle}</h1>
+        </div>
+    )
+}
+
+export default Article;
---
+++ src/containers/ArticleListing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ArticleListing(){
+    const articlesTitle = 'Article'
+    return (
+        <div>
+            <h1>{articlesTitle}</h1>
+        </div>
+    )
+}
+
+export default ArticleListing;
