759caeaa641d4d54130eb45a58e5d164c0e98728
Ty Wenrick <tew287@nyu.edu>
2021-09-16 08:28:49 -0400

    components

---
+++ src/components/ArticleCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ArticleCard(){
+    return(
+        <div>
+            <div></div>
+            <div>
+                <h2>Article Title</h2>
+            </div>
+        </div>
+    )
+}
+
+export default ArticleCard;
